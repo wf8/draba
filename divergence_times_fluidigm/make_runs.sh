c2029b94952e27402d4c720dd31b018053140a19
Will Freyman <willfreyman@gmail.com>
2017-04-17 11:08:13 -0700

    rev and savio scripts for fluidigm

---
+++ divergence_times_fluidigm/make_runs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+rm -rf runs
+mkdir runs
+
+for rep in {1..20}
+do
+
+    # base_dir = "/global/scratch/freyman/projects/draba/analyses/divergence_times_fluidigm/"
+    # run_num = 1
+    # n_iterations = 1000
+    # sample_freq = 1
+    # seed(1) 
+    
+    echo "base_dir = \"/global/scratch/freyman/projects/draba/analyses/divergence_times_fluidigm/\"" >> runs/$rep.Rev
+    echo "run_num = $rep" >> runs/$rep.Rev
+    echo "n_iterations = 1000" >> runs/$rep.Rev
+    echo "sample_freq = 1" >> runs/$rep.Rev
+    echo "seed($rep)" >> runs/$rep.Rev
+    cat draba.Rev >> runs/$rep.Rev
+    
+done
+
+
+echo "#!/bin/bash
+# Job name:
+#SBATCH --job-name=draba-all
+#
+# Partition:
+#SBATCH --partition=savio
+#
+# Processors:
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --cpus-per-task=1
+#
+# Wall clock limit (max=72:00:00)
+#SBATCH --time=72:00:00
+#
+# Account:
+#SBATCH --account=fc_rothfelslab
+#
+# Specify Faculty Computing Allowance:
+#SBATCH --qos=savio_normal
+#" > runs/draba.sh
+for core in {1..20}
+do
+    echo "/global/scratch/freyman/revbayes/projects/cmake/rb /global/scratch/freyman/projects/draba/analyses/divergence_times_fluidigm/runs/$core.Rev > /global/scratch/freyman/projects/draba/analyses/divergence_times_fluidigm/output/$core.out &" >> runs/draba.sh
+done
+echo "wait" >> runs/draba.sh
+
---
+++ divergence_times_fluidigm/submit_slurm_jobs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sbatch runs/draba.sh
